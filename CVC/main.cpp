a69601cecdaa688c0c39b1d74c6a96d6c3fb191f
ynfin <yngve@yngves-macbook-pro.local>
2016-02-15 21:42:31 +0100

    Initial Commit

---
+++ CVC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CVC
+//
+//  Created by Yngve Finnestad on 15/02/16.
+//  Copyright © 2016 Yngve Finnestad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
